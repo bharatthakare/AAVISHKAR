c58ca57a3d95b01e7c26dd453fd5b9ef732cbdd6
Bharat Thakare <bharatthakare017@gmail.com>
2025-11-15 02:31:27 +0000

    undo changes

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,7 +4,7 @@ import { Logo } from './logo';
 
 export function Footer() {
   return (
-    <footer className="bg-hover-green text-primary-foreground">
+    <footer className="bg-primary text-primary-foreground">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div className="space-y-2">
