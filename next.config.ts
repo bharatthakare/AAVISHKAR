eef4289f8fb620e33269e9f28bfaf2d43607ee20
Bharat Thakare <bharatthakare017@gmail.com>
2025-11-15 03:14:17 +0000

    08:41:59.890 Running build in Washington, D.C., USA (East) – iad1 08:41:

--- next.config.ts
+++ next.config.ts
@@ -30,10 +30,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    // This allows requests from the Firebase Studio development environment.
-    allowedDevOrigins: ["*.cloudworkstations.dev"],
-  },
 };
 
 export default nextConfig;
